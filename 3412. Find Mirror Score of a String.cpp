296070b1d11a893ca284301c5bec4a8d35f94754
Md. Arafat Kabir <99961325+Arafat2222@users.noreply.github.com>
2025-01-09 21:19:15 +0600

    Create 3412. Find Mirror Score of a String.cpp

---
+++ 3412. Find Mirror Score of a String.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long calculateScore(string str) {
+        //string str; //cin >> str;
+        map<char,char>mp;
+        vector<int>vis(1e6+123,0);
+        char z = 'z';
+        for( char ch = 'a'; ch <= 'm'; ch++ ){
+            mp[ch] = z;
+            mp[z] = ch;
+            z--;
+        }
+        stack<int>arr[27];
+        long long  ans = 0;
+        for(int i = 0; i < str.size(); i++){
+            arr[str[i] - 'a'].push(i);
+        
+            int indx = mp[str[i]] - 'a';
+            if(!arr[indx].empty() && vis[i] == 0){
+                
+                if(vis[i] == 0 && vis[arr[indx].top()] == 0){
+                    ans += abs(arr[indx].top() - i);
+                    vis[i] = 1;
+                    vis[arr[indx].top()] = 1;
+                }
+                arr[indx].pop();
+
+            }
+        }
+        return ans;
+            
+    }
+};
